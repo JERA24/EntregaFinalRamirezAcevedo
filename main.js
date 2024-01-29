842c8b212e075578370a8d1d3ce5424344443108
JERA24 <jeronimoramirezacevedo@gmail.com>
2024-01-29 17:50:25 -0300

    Segunda-preEntrega

--- main.js
+++ main.js
@@ -1,56 +1,110 @@
 //Bienvenida a la pagina 
-let nombre = prompt("Ingrese su nombre");
-alert ("Hola " + nombre + " bienvenido a nuestra pagina");
+let nombres = prompt("Ingrese su nombre");
+alert ("Hola " + nombres + " bienvenido a nuestra pagina");
 
-//Verificacion de edad para poder ingresar 
-let edad = Number(prompt("Ingrese su edad"));
+//Verificacion de edad para poder ingresar
+while (true) {
+    var edad = prompt("Por favor, ingresa tu edad:");
 
-//Mayores a 18 años para entrar
-if(edad <18){
-    alert("No se permite la venta de alcohol a menores");
-}else{
-    alert("Disfrute de nuestras promos"); 
-};
-
-// Cupon de regalo
-
-let numero = Number(prompt("Tienes un cupon de 25% de regalo, ingrese el total de su compra (mayor a 1500$):"));
-if(numero <1500){
-    alert("La compra tiene que ser mayor a 1500$")
-}else{
-    alert(`Total a pagas ${numero} $`);
+    if (edad >= 18) {
+        alert("¡Bienvenido! por favor relajate y elije lo que quieras beber");
+        break; 
+    } else {
+        alert("Lo siento, no se permite la venta de alcohol a menores, anda a jugar a la play.");
+    }
 }
 
-// Solicitar al usuario que ingrese el porcentaje
-
-let porcentaje = prompt("Ingresa el porcentaje del cupon de regalo:");
-if(porcentaje != 25){
-    alert("No tienes ningun cupon.")
+// fun bebidas
+function Bebida(nombre, precio) {
+    this.nombre = nombre;
+    this.precio = precio;
 }
 
-// Calcular el porcentaje
+// bebidas
+const listaBebidas = [
+    new Bebida('Aperol', 11500),
+    new Bebida('Vino', 9000),
+    new Bebida('Gancia', 6500),
+    new Bebida('Whisky', 25000),
+    new Bebida('Fernet', 7500),
+    new Bebida('Ron', 8000),
+    new Bebida('Cerveza', 2450),
+    new Bebida('Tequila', 2000),
+    new Bebida('Champagne', 7200),
+    new Bebida('Jagger', 15000),
+    new Bebida('Gin', 19000),
+];
 
-let resultado = (numero * porcentaje) / 100;
+// Carrito 
+const carrito = [];
 
-alert(`Gracias a tu cupon de ${porcentaje}% , a tu total de ${numero} $ se le descuenta ${resultado} $`);
+//primera compra true
+let primeraCompra = true;
 
-//Moneda con la que va a pagar
-let compra= prompt("Ingrese con que moneda vas a pagar").toUpperCase()
-
-switch(compra){
-    case "USD":
-        alert(`${nombre} va a pagar con Dolares`);
-        break;
-    case "ARG":
-        alert(`${nombre} quiere pagar con peso Argentino`);
-        break;
-    case "EUR":
-        alert(`${nombre} va a pagar con Euros`);
-        break;
-    default:
-        alert("Moneda no reconocida");
-        break;
+// buscar bebida
+function agregarAlCarritoPorNombre(nombreBebida) {
+    const bebidaEncontrada = listaBebidas.find(bebida => bebida.nombre.toLowerCase() === nombreBebida.toLowerCase());
+    if (bebidaEncontrada) {
+        carrito.push(bebidaEncontrada);
+        alert(`Se ha agregado ${bebidaEncontrada.nombre} al carrito.`);
+    } else {
+        alert(`${nombreBebida} no está disponible.`);
+    }
 }
+
+// fun carrito
+function mostrarCarrito() {
+    if (carrito.length === 0) {
+        alert("El carrito está vacío.");
+    } else {
+        alert("Contenido del carrito:");
+        let total = 0;
+        carrito.forEach(bebida => {
+            alert(`${bebida.nombre} - Precio: $ ${bebida.precio} `);
+            total += bebida.precio;
+        });
+        
+        // descuento en la primera compra
+        if (primeraCompra) {
+            total *= 0.75; 
+            alert("¡Felicidades por tu primera compra! Se aplicó un descuento del 25%.");
+            
+        }
+        
+        alert(`Total de la compra: $ ${total}`);
+
+        // tarjeta o efectivo
+
+        let metodoPago = prompt("¿Cómo deseas pagar? Ingresa 'tarjeta' para pagar con tarjeta o 'efectivo' para pagar en efectivo:");
+        metodoPago = metodoPago.toLowerCase();
+  
+        if (metodoPago === 'tarjeta') {
+            alert("Se ha seleccionado pago con tarjeta.");
+            
+        } else if (metodoPago === 'efectivo') {
+            alert("Se ha seleccionado pago en efectivo.");
+            
+        } else {
+          alert("Método de pago no válido.");
+        }
+      }
+}
+
+//  agregar bebidas
+let agregarOtraBebida = true;
+while (agregarOtraBebida) {
+    let nombreBebida = prompt("Ingresa el nombre de la bebida que deseas agregar al carrito:");
+    agregarAlCarritoPorNombre(nombreBebida);
+
+    let respuesta = prompt("¿Deseas agregar otra bebida? (si/no)");
+    if (respuesta.toLowerCase() !== 'si') {
+        agregarOtraBebida = false;
+    }
+}
+
+// contenido final del carrito y total de compra
+mostrarCarrito();
+
 
 //Listado solo para usuarios prime
 let usuario = prompt("Si pagas el listado prime por favor ingrese el usuario").toUpperCase();
@@ -71,31 +125,6 @@ for(let mesa = 1; mesa <= 2; mesa++){
     alert(`Mesa #${mesa} reservado para ${nombre}`);
 
 };
-
-//Calculadora para no salir de la pagina
-function calculadora(numUno, numDos, operacion){
-    switch (operacion){
-        case "+":
-            return numUno + numDos;
-        case "-":
-            return numUno - numDos;
-        case "*":
-            return numUno * numDos;   
-        case "/":
-            return numUno / numDos;                             
-    }
-}
-
-let numeroUno = Number(prompt("Te facilitamos una calculadora para ti, ingrese un numero:"));
-let operacion = prompt("Ingrese la operacion a realizar");
-let numeroDos = Number(prompt("ingrese otro numero:"));
-let total = calculadora (numeroUno, numeroDos, operacion);
-
-alert(`${numeroUno} ${operacion} ${numeroDos} = ${total}`);
-
-
-
-
 
 
 
